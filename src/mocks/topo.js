d4adbb625305ab7a952e262c8359eb0b534e5865
Gabriel-Augusto-do-Vale <gabrielaugusto.vale2005@gmail.com>
2025-09-18 21:25:43 -0300

    aula 2

---
+++ src/mocks/topo.js
@@ -0,0 +1,6 @@
+export const topo = {
+    boasVindas: "Olá, Gabriel!",
+    legenda: "Encontre os melhores produtores.",
+}
+
+export default topo;
--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,14 +1,60 @@
 import React from "react";
-import {Image, View, Text } from "react-native";
+import {Image, View, Text, StyleSheet} from "react-native";
+import { carregaTopo } from "../../../services/carregaDados";
 
 import logo from "../../../assets/logo.png";
 
-export default function Topo() {
-  return (
-    <View>
-        <Image source={logo} />
-        <Text>Olá, Carlos!</Text>
-        <Text>Encontre os melhores produtores.</Text>
-    </View>
-  );
+class Topo extends React.Component{
+  state = {
+    topo: {
+      boasVindas: "",
+      legenda: "",
+    },
+  };
+
+  atualizaTopo(){
+    const retorno = carregaTopo();
+    this.setState({topo: retorno});
+  }
+
+  componentDidMount(){
+    this.atualizaTopo();
+  }
+
+  render(){
+    return(
+      <View style={estilos.topo}>
+          <Image source={logo} style={estilos.img} />
+          <Text style={estilos.boasVindas}>{this.state.topo.boasVindas}</Text>
+          <Text style={estilos.legenda}>{this.state.topo.legenda}</Text>
+      </View>
+    )
 }
+}
+
+
+const estilos = StyleSheet.create({
+  topo:{
+    backgroundColor:"#F6F6F6",
+    padding: 16,
+  },
+
+  img:{
+    width: 70,
+    height: 28,
+  },
+
+  boasVindas:{
+    marginTop: 24,
+    lineHeight: 42,
+    fontSize: 26,
+    fontWeight: 600,
+  },
+
+  legenda:{
+    fontSize:16,
+    lineHeight: 16,
+  }
+})
+
+export default Topo;
