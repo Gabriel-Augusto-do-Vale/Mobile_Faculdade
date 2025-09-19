111d58d8b111fce7e3c797e17ced53d003400df8
Gabriel-Augusto-do-Vale <gabrielaugusto.vale2005@gmail.com>
2025-09-18 21:59:04 -0300

    aula 2

---
+++ src/mocks/produtores.js
@@ -0,0 +1,47 @@
+import green from "../assets/produtores/green.png"
+import grow from "../assets/produtores/grow.png"
+import jennyjack from "../assets/produtores/jenny-jack.png"
+import potage from "../assets/produtores/potager.png"
+import salad from "../assets/produtores/salad.png"
+
+const produtores = {
+    titulo: "Produtores",
+    lista: [
+        {
+            nome: "Green",
+            imagem: green,
+            distancia: "1m",
+            estrelas: '5',
+        },
+
+        {
+            nome: "Salad",
+            imagem: salad,
+            distancia: "1m",
+            estrelas: '5',
+        },
+
+        {
+            nome: "Jenny-Jack",
+            imagem: jennyjack,
+            distancia: "1m",
+            estrelas: '5',
+        },
+
+        {
+            nome: "Grow",
+            imagem: grow,
+            distancia: "1m",
+            estrelas: '5',
+        },
+
+        {
+            nome: "Potager",
+            imagem: potager,
+            distancia: "1m",
+            estrelas: '5',
+        }
+    ]
+}
+
+export default produtores;
---
+++ src/telas/Home/componentes/Produtores.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {FlatList, View, Text, StyleSheet} from "react-native";
+import { carregaProdutores } from "../../../services/carregaDados";
+
+export default function Produtores(){
+    return(
+        <FlatList
+        />
+    )
+};
